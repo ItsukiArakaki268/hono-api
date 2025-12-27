fee3ff0ef7eb7a72089bb54e6fca2c9868481bab
ItsukiArakaki268 <itukiprog@gmail.com>
2025-12-27 16:14:23 +0900

    不要なファイルを削除

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,5 @@
 import { Hono } from "hono";
-import posts from "./routes/posts";
 import admin from "./routes/admin";
-import api from "./routes/api";
 import { View } from "./components/view";
 import tasks from "./routes/tasks";
 import db from "./routes/db";
@@ -20,9 +18,7 @@ app.get("/page", (c) => {
   return c.html(<View />);
 });
 
-app.route("/posts", posts);
 app.route("/admin", admin);
-app.route("/api", api);
 app.route("/tasks", tasks);
 app.route("/db", db);
 
--- src/routes/tasks.ts
+++ src/routes/tasks.ts
@@ -1,5 +1,4 @@
 import { Hono } from "hono";
-// import { tasks, generatedId, Task } from "../data/tasks";
 import { neon } from "@neondatabase/serverless";
 
 type Bindings = {
